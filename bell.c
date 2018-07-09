4f00256d1538b1fd40ef4a35e2e439d66f89dc8c
Andy THEVENET <athevene@e1r11p19.42.fr>
2018-07-10 01:44:52 +0200

    Bell

---
+++ bell.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bell.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athevene <thevenetandy@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/10 01:39:30 by athevene          #+#    #+#             */
+/*   Updated: 2018/07/10 01:41:13 by athevene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+	while (1)
+		printf("Belling... Please lock session before taking a break ! Next time is T.I.G.%c\n", 7);
+}
